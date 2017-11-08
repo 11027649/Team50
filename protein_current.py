64c7a1d33288f8f6c29bd3fb577414824a7d7395
[11027649] <[natasja.wezel@hotmail.com]>
2017-11-08 15:46:23 +0100

    tried to split print and make grid function in two functions. still a little buggy

--- protein_current.py
+++ protein_current.py
@@ -7,12 +7,20 @@ init()
 protein = []
 protein_length = 0
 ALLOWED_LENGTH = 20
+min_x = 0
+min_y = 0
+x = 0
+y = 0
+
+aa_info = []
 
 # save position of each amino acid to remember which ones are linked
 class Amino():
-    def __init__(self, letter, pos_next):
+    def __init__(self, letter, pos_next, aa_x, aa_y):
         self.letter = letter
         self.pos_next = pos_next
+        self.aa_x = aa_x
+        self.aa_y = aa_y
 
 
 def init_protein():
@@ -20,7 +28,9 @@ def init_protein():
 
     # ask user for protein string as input, max length
     protein_string = input("Insert protein string: ")
+    
     global protein_length
+
     protein_length = len(protein_string)
 
     # check if protein isn't too long
@@ -35,11 +45,12 @@ def init_protein():
             exit(1)
 
     # set all directions of the amino acids to Continue, and if it's the last one, to End
+    # give the protein coordinates: y = 0 and x = 0 + i
     for i in range(protein_length):
         if i == protein_length - 1:
-            protein.append(Amino(protein_string[i].upper(), 'E'))
+            protein.append(Amino(protein_string[i].upper(), 'E', i, 0))
         else:
-            protein.append(Amino(protein_string[i].upper(), 'C'))
+            protein.append(Amino(protein_string[i].upper(), 'C', i, 0))
 
 
 def fold_protein(amino_number, direction):
@@ -55,16 +66,9 @@ def fold_protein(amino_number, direction):
     # if not, write direction into amino acid
     protein[amino_number].pos_next = direction
 
-
-def print_protein():
-    """ Prints the protein in a nice way. """
-
-    # make array for coordinates
-    coordinates = []
+def initialize_grid():
 
     # initialize coordinates
-    min_x = 0
-    min_y = 0
     max_x = 0
     max_y = 0
 
@@ -74,7 +78,10 @@ def print_protein():
 
     # 1(right) 2(down) 3(left) 0(up), standard direction is to the right
     direction = 1
-    
+
+    print('Hallo ik ben in initialize_grid aangekomen')
+    print(protein_length)
+
     # for each amino acid in the protein
     for i in range(protein_length):
 
@@ -94,6 +101,9 @@ def print_protein():
         if x_pos > max_x:
             max_x = x_pos
 
+        global min_y
+        global min_x
+
         if y_pos < min_y:
             min_y = -abs(y_pos)
         if x_pos < min_x:
@@ -104,39 +114,63 @@ def print_protein():
             direction -= 1
         if protein[i].pos_next == 'R':
             direction += 1
+        
         # makes sure the direction is the right format (never above 3)
         direction %= 4;
 
         # append the amino acids and its coordinates on the grid
-        coordinates.append([x_pos, y_pos, protein[i].letter, direction])
+        print(protein[i].letter)
+        
+        protein[i].aa_x = x_pos
+        protein[i].aa_y = y_pos
 
-    # gets a whitespace between the inserted protein and the print
-    print()
+        aa_info.append([x_pos, y_pos, protein[i].letter, direction])
 
+    global x
     x = min_x + max_x + 1
+    global y
     y = abs(min_y) + max_y + 1
+    
 
-    # initialize the grid
+
+def visualize_fold():
+    # make a grid twice the size of the former grid
     grid = [['  ' for p in range(y * 2 - 1)] for q in range(x * 2 - 1)]
+
+    print('this is min x en min y', end='')
+    print(min_x, min_y)
+
+    print(str(protein_length) + 'in visualize_fold')
+    
     for i in range(protein_length):
 
-        coor_x = (coordinates[i][0] + min_x) * 2
-        coor_y = (coordinates[i][1] + abs(min_y)) * 2
+        coor_x = (aa_info[i][0] + min_x) * 2
+        coor_y = (aa_info[i][1] + abs(min_y)) * 2
+        print(coor_x)
+        print(coor_y)
+        # the coordinates are 
+        grid[coor_x][coor_y] = aa_info[i][2]
 
-
-        grid[coor_x][coor_y] = [coordinates[i][2], coordinates[i][3]]
-
-    # add layout, making the grid 2 times as big, to add bonds
-        # makes sure the last protein does not print its extra layout
+        # print extra layout for all AA except the last one
         if i != protein_length - 1:
-            if coordinates[i][3] == 0:
+
+            # pipeline under it
+            if aa_info[i][3] == 0:
                 grid[coor_x][coor_y - 1] = '| '
-            elif coordinates[i][3] == 1:
+
+            # stripes right to it
+            elif aa_info[i][3] == 1:
                 grid[coor_x + 1][coor_y] = '---'
-            elif coordinates[i][3] == 2:
+            
+            # pipeline above it
+            elif aa_info[i][3] == 2:
                 grid[coor_x][coor_y + 1] = '| '
-            elif coordinates[i][3] == 3:
+            
+            # stripes left to it
+            elif aa_info[i][3] == 3:
                 grid[coor_x - 1][coor_y] = '---'
+
+            print('klaar')
 
     # print the grid
     for i in range(y * 2 - 1):
@@ -149,16 +183,16 @@ def print_protein():
                 if grid[j][i][0] == 'H':
                     print('\033[34;1m' + grid[j][i][0] + '\033[0m', end='')
 
-                    if grid[j][i][1] != 1 and j + 1 < x * 2 - 1 and grid[j + 1][i] != "---":
-                        print(' ', end='')
+                    #if grid[j][i][1] != 1 and j + 1 < x * 2 - 1 and grid[j + 1][i] != "---":
+                     #   print(' ', end='')
 
 
                 # print P red
                 elif grid[j][i][0] == 'P':
                     print('\033[31;1m' + grid[j][i][0] + '\033[0m', end='')
 
-                    if grid[j][i][1] != 1 and j + 1 < x * 2 - 1 and grid[j + 1][i] != "---":
-                        print(' ', end='')
+                    #if grid[j][i][1] != 1 and j + 1 < x * 2 - 1 and grid[j + 1][i] != "---":
+                     #   print(' ', end='')
 
 
                 # if something else is present
@@ -167,6 +201,7 @@ def print_protein():
 
         print()
     print()
+
 
 
 def clear_screen():
@@ -181,18 +216,24 @@ def clear_screen():
 
 def main():
     init_protein()
+    initialize_grid()
 
+    print(y)
+    print(x)
+    visualize_fold()
 
-    fold_protein(1, 'L')
-    fold_protein(2, 'L')
-    fold_protein(3, 'R')
-    fold_protein(4, 'R')
-    fold_protein(6, 'R')
-    fold_protein(9, 'R')
-    fold_protein(12, 'R')
-    fold_protein(16, 'R')
+    fold_protein(3, 'L')
+    initialize_grid()
+    visualize_fold()
 
-    print_protein()
+    # fold_protein(2, 'L')
+    # initialize_grid(min_x, min_y)
+    # visualize_fold(min_x, min_y, x, y, aa_info)
+
+    # fold_protein(3, 'R')
+    # initialize_grid(min_x, min_y)
+    # visualize_fold(min_x, min_y, x, y, aa_info)
+
 
 
 if __name__ == "__main__":
