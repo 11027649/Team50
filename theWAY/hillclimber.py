4ee6bab8b81ee0eeb8b714d4688fca34fbd62240
[11027649] <[natasja.wezel@hotmail.com]>
2017-11-23 17:01:55 +0100

    Wrote a method to keep track on how fast our hillclimber stabilizes

--- theWAY/hillclimber.py
+++ theWAY/hillclimber.py
@@ -5,6 +5,7 @@ from print_protein import print_protein
 import time
 from random import randint
 import copy
+import csv
 
 import global_vars
 global_vars.init()
@@ -17,23 +18,32 @@ def hillclimber():
     length = len(global_vars.protein_string)
     global_vars.winning_grid = copy.deepcopy(global_vars.grid)
     global_vars.winning_coordinates = copy.deepcopy(global_vars.coordinates)
-    # do 1000 random folds and keep track of the highest value
-    for i in range(10000):
 
-        for j in range(5):
-            random_value = getrandvalue()
-            fold(random_value[0], random_value[1])
 
-        # if the score is lower save that particular grid in winning grid
-        if score() < best_score:
-            global_vars.winning_grid = copy.deepcopy(global_vars.grid)
-            global_vars.winning_coordinates = copy.deepcopy(global_vars.coordinates)
-            best_score = score()
-            print_protein()
-            print(score())
-        else:
-            global_vars.grid = copy.deepcopy(global_vars.winning_grid)
-            global_vars.coordinates = copy.deepcopy(global_vars.winning_coordinates)
+    with open('data.csv', 'w', newline='') as csvfile:
+        datawriter = csv.writer(csvfile)
+
+        # do 1000 random folds and keep track of the highest value
+        for i in range(1000):
+
+            # store data in .csv
+            datawriter.writerow([i] + [best_score])
+            
+            for j in range(5):
+                random_value = getrandvalue()
+                fold(random_value[0], random_value[1])
+
+            # if the score is lower save that particular grid in winning grid
+            if score() < best_score:
+                global_vars.winning_grid = copy.deepcopy(global_vars.grid)
+                global_vars.winning_coordinates = copy.deepcopy(global_vars.coordinates)
+                best_score = score()
+                print_protein()
+                print(score())
+
+            else:
+                global_vars.grid = copy.deepcopy(global_vars.winning_grid)
+                global_vars.coordinates = copy.deepcopy(global_vars.winning_coordinates)
 
 
 # return an array with a random direction and aminonumber
