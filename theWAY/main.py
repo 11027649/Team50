9e365ea125c0e7e88444c8e24ea4698e774ebf0d
Segouta <christianbijvoets@gmail.com>
2017-11-24 10:08:32 +0100

    User friendlyness improved

--- theWAY/main.py
+++ theWAY/main.py
@@ -6,25 +6,30 @@ from update_grid import update_grid
 from fold import fold
 from score import score
 from hillclimber import hillclimber
-
 from algo_brute_force import algo_brute_force
 
+# Import all the global variables.
 import global_vars
 global_vars.init()
 
 message("This is a protein-fold-optimizer by Team50")
 
+# Get the user's choice of protein.
 input_string()
 
 message("Protein received")
 
+# Print starting configuration of the protein
 print_protein()
 
 message("Protein initiated, starting algorithm.")
 
 message("Folding summary:")
 
-hillclimber()
-message("We found " + str(global_vars.amount) + " best solutions.")
+""">>>>>> UNCOMMENT THE ALGORITHM YOU WANT TO USE BELOW <<<<<<"""
 
+# algo_brute_force()
+# hillclimber()
+
+# Print the best solution.
 print_protein()
