d10c0c93e1ac7b7f0616525d8c0cb8a67b3f7573
[11027649] <[natasja.wezel@hotmail.com]>
2017-11-28 15:45:06 +0100

    Extracted the recursive function from the brute force function. And deleted the 'alogoritm' file.

--- theWAY/algo_brute_force.py
+++ theWAY/algo_brute_force.py
@@ -1,8 +1,5 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# Contains: algo_brute_force, recursive_function, fold_all_right.
-#
-# Fold all right makes a spiral of the protein. This was to try if the fold and
-# score function worked right.
+# Contains: algo_brute_force, recursive_function.
 #
 # The algo_brute_force tries every foldation of the protein.
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -18,9 +15,7 @@ import copy
 import global_vars
 global_vars.init()
 
-
-
-def algo_brute_force():
+def brute_force():
     """ This algorithm tries every possibility of foldation of the protein.
         Therefore it uses the global coordinates. It stores the amount of "best"
         solutions in a global variable called amount.
@@ -29,51 +24,24 @@ def algo_brute_force():
 
     print("Brute forcing...")
 
-    def recursive_function(depth):
-
-        current_score = score()
-
-        if current_score < global_vars.winning_score:
-
-            global_vars.winning_score = current_score
-            global_vars.winning_grid = copy.deepcopy(global_vars.grid)
-            global_vars.winning_coordinates = copy.deepcopy(global_vars.coordinates)
-            global_vars.amount += 1
-            # print(winning_coordinates)
-            print("\n\nBest so far, stability of " + str(global_vars.winning_score) + ":\n")
-            print_protein()
-
-
-        if (depth < 1):
-            return
-
-        fold(depth, "L")
-        recursive_function(depth - 1)
-        fold(depth, "R")
-        recursive_function(depth - 1)
-        fold(depth, "R")
-        recursive_function(depth - 1)
-        fold(depth, "L")
-
-
     length = len(global_vars.protein_string)
     depth = length - 2
     steps = pow(3, len(global_vars.protein_string) - 2)
     sps = 3000
 
-    if input("This brute force algorithm will take " + str(steps) + " steps. \nThat equals approx. "+ str(int(steps / (sps * 60))) +"m "+ str(round(60*((steps / (sps * 60)) % 1), 2)) +"s.\n U sure? (y/n) ").upper() == "Y":
+    if input("This brute force algorithm will take " + str(steps) + " steps. \nThat equals approx. "+ str(int(steps / (sps * 60))) +"m "+ str(round(60*((steps / (sps * 60)) % 1), 2)) +"s.\n Are you sure you want to start? (y/n) ").upper() == "Y":
         print()
 
         start = time.time()
         # fold_all_right()
-        recursive_function(depth)
+        foldings_recursive(depth)
 
         end = time.time()
 
         print("Best solution found with a stability of " + str(global_vars.winning_score) + " in: " + str(int((end - start) / 60)) + "m " + str(round(((end - start) % 60), 2)) + "s.")
         print()
 
-        # print(winning_coordinates)
+        # save winning coordinates and grid
         global_vars.grid = global_vars.winning_grid
         global_vars.coordinates = global_vars.winning_coordinates
 
@@ -81,7 +49,28 @@ def algo_brute_force():
         print("\nStopping program\n")
         exit(1)
 
+def foldings_recursive(depth):
 
-def fold_all_right():
-    for i in range(1, len(global_vars.protein_string) - 1):
-        fold(i, "R")
+    current_score = score()
+
+    if current_score < global_vars.winning_score:
+
+        global_vars.winning_score = current_score
+        global_vars.winning_grid = copy.deepcopy(global_vars.grid)
+        global_vars.winning_coordinates = copy.deepcopy(global_vars.coordinates)
+        global_vars.amount += 1
+        # print(winning_coordinates)
+        print("\n\nBest so far, stability of " + str(global_vars.winning_score) + ":\n")
+        print_protein()
+
+
+    if (depth < 1):
+        return
+
+    fold(depth, "L")
+    foldings_recursive(depth - 1)
+    fold(depth, "R")
+    foldings_recursive(depth - 1)
+    fold(depth, "R")
+    foldings_recursive(depth - 1)
+    fold(depth, "L")
