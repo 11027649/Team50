d31bb113c44ce70c335e441e7ebeee8330abfa6e
[11027649] <[natasja.wezel@hotmail.com]>
2017-11-28 17:44:43 +0100

    restored

---
+++ theWAY/main.py
@@ -0,0 +1,41 @@
+from message import message
+from input_string import input_string
+from print_protein import print_protein, debug_print_protein
+from update_grid import update_grid
+from fold import fold
+from score import score
+from hillclimber import hillclimber
+from plots import plothillclimber
+import csv
+
+# Import all the global variables.
+import global_vars
+global_vars.init()
+
+
+def main():
+    message("This is a protein-fold-optimizer by Team50")
+
+    # Get the user's choice of protein.
+    input_string()
+
+    message("Protein received")
+
+    # Print starting configuration of the protein
+    debug_print_protein()
+
+    message("Protein initiated, starting algorithm.")
+
+    """>>>>>> UNCOMMENT THE ALGORITHM YOU WANT TO USE BELOW <<<<<<"""
+
+
+    # Print the best solution.
+    debug_print_protein()
+    print_protein()
+    print("Score: " + str(global_vars.winning_score), end = '\n\n')
+
+    plothillclimber()
+
+
+if __name__ == '__main__':
+	main()
