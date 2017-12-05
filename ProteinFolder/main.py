6dbbf206c852965802f015b932920f7061e614ab
[11027649] <[natasja.wezel@hotmail.com]>
2017-12-05 10:11:47 +0100

    main aangepast

--- ProteinFolder/main.py
+++ ProteinFolder/main.py
@@ -27,18 +27,27 @@ def main():
     # Print starting configuration of the protein
     print_protein()
 
+    algorithm = 0
+
+    while not algorithm == 1 and not algorithm == 2 and not algorithm == 3:
+        algorithm = int(input("Type 1 for Brute Force \n Type 2 for Hill Climber \n Type 3 for Hill Climber with Simulated Annealing \n What algorithm do you want to use? "))
+
     message("Protein initiated, starting algorithm.")
 
-    """>>>>>> UNCOMMENT THE ALGORITHM YOU WANT TO USE BELOW <<<<<<"""
-    # brute_force()
+    if algorithm == 1:
+        brute_force()
 
-    # hillclimber()
+    if algorithm == 2:
+        hillclimber()
+
+    if algorithm == 3:
+        print("This algorithm hasn't been implemented yet!")
     
     print_protein()
     print("Score: " + str(global_vars.winning_score), end = '\n\n')
     
-    """>>>>> UNCOMMENT THIS IF YOU WANT TO RUN A HILLCLIMBER ÁND PLOT THE DATA <<<<<<"""
-    # plothillclimber()
+    if algorithm == 2:
+        plothillclimber()
 
 
 
