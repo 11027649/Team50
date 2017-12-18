c022c8ccd835c77ee0ea8fa0dfeeee54c1124487
11027649 <natasjawezel@hotmail.com>
2017-12-18 14:44:40 +0100

    scripts voor data

---
+++ run_all_HC_FC_3D.py
@@ -0,0 +1,90 @@
+from protein_class import Protein
+
+# import algorithms
+from algorithms.HC_FC import hillclimber3D
+
+import csv
+
+def main():
+
+    with open('B2_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "HPHPPHHPHPPHPHHPPHPH"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)          
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('B3_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "PPPHHPPHHPPPPPHHHHHHHPPHHPPPPHHPPHPP"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)          
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('B4_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "HHPHPHPHPHHHHPHPPPHPPPHPPPPHPPPHPPPHPHHHHPHPHPHPHH"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)  
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('C1_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "PPCHHPPCHPPPPCHHHHCHHPPHHPPPPHHPPHPP"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)     
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('C2_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "CPPCHPPCHPPCPPHHHHHHCCPCHPPCPCHPPHPC"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)            
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('C3_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "HCPHPCPHPCHCHPHPPPHPPPHPPPPHPCPHPPPHPHHHCCHCHCHCHH"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)          
+            datawriter.writerow([i] + [protein.winning_score])
+
+    with open('C4_3D_HC_FC.csv', 'a', newline='') as datafile:
+        datawriter = csv.writer(datafile)
+
+        for i in range(50):
+            print("Piep: " + str(i))
+            protein_string = "HCPHPHPHCHHHHPCCPPHPPPHPPPPCPPPHPPPHPHHHHCHPHPHPHH"
+            protein = Protein(protein_string.upper(), len(protein_string), [], [], [], [], 0, [])
+            protein.init_grid()
+            protein = hillclimber3D(protein)        
+            datawriter.writerow([i] + [protein.winning_score])
+
+
+if __name__ == '__main__':
+	main()
+
